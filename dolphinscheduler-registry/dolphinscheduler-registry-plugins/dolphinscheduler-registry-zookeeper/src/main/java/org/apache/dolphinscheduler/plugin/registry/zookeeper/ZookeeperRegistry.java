997f7c8282906685dd962f9ccf7ee3c8a7c36835
johnnyzhou <johnny.zhou@163.com>
2025-12-06 15:36:07 +0800

    npe fix

--- dolphinscheduler-registry/dolphinscheduler-registry-plugins/dolphinscheduler-registry-zookeeper/src/main/java/org/apache/dolphinscheduler/plugin/registry/zookeeper/ZookeeperRegistry.java
+++ dolphinscheduler-registry/dolphinscheduler-registry-plugins/dolphinscheduler-registry-zookeeper/src/main/java/org/apache/dolphinscheduler/plugin/registry/zookeeper/ZookeeperRegistry.java
@@ -233,7 +233,7 @@ public final class ZookeeperRegistry implements Registry {
 
     @Override
     public boolean releaseLock(String key) {
-        if (null == threadLocalLockMap.get().get(key)) {
+        if (null == threadLocalLockMap.get() || null == threadLocalLockMap.get().get(key)) {
             return false;
         }
         try {
